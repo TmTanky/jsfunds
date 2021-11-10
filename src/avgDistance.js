77d7d20832ed2a060fcc5f29592882615b0067b7
Joshua-Chu <jpchu@up.edu.ph>
2021-11-10 19:06:43 +0800

    Added 2 js problems

---
+++ src/avgDistance.js
@@ -0,0 +1,26 @@
+const arrObj = [
+	{
+		name: "Joshua",
+		initialLoc: "200",
+		finalLoc: 400,
+	},
+	{
+		name: "Aji",
+		initialLoc: 400,
+		finalLoc: 500,
+	},
+	{
+		name: "Evangel",
+		initialLoc: "150",
+		finalLoc: "300",
+	},
+];
+
+// Output 150
+
+const avgDistance = (arrObj) => {
+	// Code here
+	return 0;
+};
+
+console.log(avgDistance(arrObj));
